5ae6797decb01ff9bee2331a5a5bfea06a32690c
Michael Tarleton <michael.tarleton@me.com>
2019-08-08 13:54:11 -0500

    Added the data-grid component

--- src/app/dashboards/dashboards.module.ts
+++ src/app/dashboards/dashboards.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DataGridComponent } from '../data-grid/data-grid.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [DataGridComponent],
   imports: [
     CommonModule
   ]
