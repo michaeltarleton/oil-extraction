4525215bcf3e7f450e3bdee138893601fdb01ac3
Michael Tarleton <michael.tarleton@me.com>
2019-08-08 13:28:24 -0500

    Added sample flexmonster pivot table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { environment } from '../environments/environment';
+
 
 @Component({
   selector: 'app-root',
@@ -6,4 +8,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  flexMonsterKey: string = environment.flexMonsterTrialKey;
 }
