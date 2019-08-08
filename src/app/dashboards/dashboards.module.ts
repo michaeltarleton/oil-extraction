2150bb6420825bcd4f306cf9d35c5968239af0c0
Michael Tarleton <michael.tarleton@me.com>
2019-08-08 14:11:12 -0500

    Attached the local datasource to the first grid

--- src/app/dashboards/dashboards.module.ts
+++ src/app/dashboards/dashboards.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DataGridComponent } from './data-grid/data-grid.component';
+import { FlexmonsterPivotModule } from 'ng-flexmonster';
 
 
 
 @NgModule({
   declarations: [DataGridComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FlexmonsterPivotModule
   ],
   exports: [DataGridComponent]
 })
